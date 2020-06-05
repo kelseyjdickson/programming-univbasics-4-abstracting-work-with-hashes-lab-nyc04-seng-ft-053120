6eacb4c437020847c9a126cda1245604267c838c
Kelsey Dickson <kelseymedved@gmail.com>
2020-06-05 13:49:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   { key => value }
@@ -14,6 +16,7 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   if  hash[key]
     hash[key] = 1
+    binding.pry
   else
 
   end
